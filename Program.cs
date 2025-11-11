73d4f8d72d83097e0baad3ec9438e9aadbb9d7d4
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-11 09:00:48 +0100

    Use alternate buffer

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Console setup
+        Console.Write("\x1b[?1049h\x1b[H"); // Alternate buffer
+        Console.CursorVisible = false;
+
+        Console.SetCursorPosition(Console.BufferWidth / 2, Console.BufferHeight / 2);
+        Console.Write('@');
+        Thread.Sleep(2200);
+        Console.Write("\x1b[?1049l"); // Main buffer
     }
 }
