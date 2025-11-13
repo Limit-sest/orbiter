3e8b11c52a46678fab8b686d10236d6e0b774801
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-13 13:28:03 +0100

    Add planet paths

--- Program.cs
+++ Program.cs
@@ -10,37 +10,118 @@ class Program
             Console.Write('@');
         }
     }
-    
+
     class Planet
     {
+        class PathPoint
+        {
+            public int X;
+            public int Y;
+            public char? Symbol;
+
+            public PathPoint(int x, int y, char? symbol = ' ')
+            {
+                X = x;
+                Y = y;
+                Symbol = symbol;
+            }
+        }
+
         private char symbol;
         private int x;
         private int y;
         private int radius;
         private double angle = 0;
         private double speed;
+        private List<PathPoint> path = new List<PathPoint>();
 
-        public Planet(char symbol, int radius, double speed){
+        public Planet(char symbol, int radius, double speed)
+        {
             this.symbol = symbol;
             this.radius = radius;
             this.speed = speed;
         }
-        
-        private void SetPos(){
-            this.x = Console.BufferWidth / 2 + (int)Math.Round(Math.Cos(this.angle) * this.radius * 2.5);
-            this.y = Console.BufferHeight / 2 + (int)Math.Round(Math.Sin(this.angle) * this.radius * 0.8);
+
+        public void GeneratePath()
+        {
+            // Generate coords
+            for (double i = 0; i < 360; i += 0.01)
+            {
+                (int x, int y) = GetPos(i, this.radius);
+                // Skip if same coords
+                if (path.Count() > 0)
+                {
+                    var previousPoint = path.Last();
+                    if ((x, y) == (previousPoint.X, previousPoint.Y)) continue;
+                }
+                path.Add(new PathPoint(x, y));
+            }
+            // Assign symbols
+            for (int i = 0; i < path.Count(); i++)
+            {
+                var currentPoint = path[i];
+                PathPoint previousPoint;
+                PathPoint nextPoint;
+                if (i - 1 < 0)
+                {
+                    previousPoint = path.Last();
+                }
+                else
+                {
+                    previousPoint = path[i - 1];
+                }
+
+                if (i + 1 < path.Count())
+                {
+                    nextPoint = path[i + 1];
+                }
+                else
+                {
+                    nextPoint = path[0];
+                }
+
+                currentPoint.Symbol = (previousPoint.X.CompareTo(nextPoint.X), previousPoint.Y.CompareTo(nextPoint.Y)) switch
+                {
+                    (1, -1) => '┘',
+                    (-1, -1) => '┐',
+                    (1, 1) => '└',
+                    (-1, 1) => '┌',
+                    (1, 0) => '─',
+                    (-1, 0) => '─',
+                    (0, 1) => '│',
+                    (0, -1) => '│',
+
+                    _ => 'x'
+                };
+            }
         }
-        
-        public void ProcessTick(){
-            SetPos();
+
+
+        private (int x, int y) GetPos(double angle, int radius)
+        {
+            x = Console.BufferWidth / 2 + (int)Math.Round(Math.Cos(angle) * radius * 2.5);
+            y = Console.BufferHeight / 2 + (int)Math.Round(Math.Sin(angle) * radius * 0.8);
+            return (x, y);
+        }
+
+        public void ProcessTick()
+        {
+            foreach (PathPoint point in path)
+            {
+                Console.SetCursorPosition(point.X, point.Y);
+                Console.Write(point.Symbol);
+            }
+
+            (this.x, this.y) = GetPos(this.angle, this.radius);
             angle -= speed;
-            if (angle <= 0) {
+            if (angle <= 0)
+            {
                 angle = 360;
             }
             Console.SetCursorPosition(this.x, this.y);
             Console.Write(symbol);
         }
-        
+
     }
 
     private static volatile bool _running = true;
@@ -63,17 +144,23 @@ class Program
         // Start asynchronous keypress handler
         var keyTask = Task.Run(() => HandleKeyPress());
 
+        foreach (Planet planet in planets)
+        {
+            planet.GeneratePath();
+        }
+
         try
         {
             while (_running)
             {
-                // Console.Clear();
+                Console.Clear();
                 sun.ProcessTick();
-                foreach (Planet planet in planets){
+                foreach (Planet planet in planets)
+                {
                     planet.ProcessTick();
                 }
                 DrawControls();
-                await Task.Delay(30);
+                await Task.Delay(500);
             }
         }
         finally
